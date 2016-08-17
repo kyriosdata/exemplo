406c0f5bde95892ea4ad0355ad1bdea5d3080bdb
kyriosdata <fabio@inf.ufg.br>
2016-08-17 13:18:19 -0300

    Programa para ilustrar uso da classe.

---
+++ src/main/java/com/github/kyriosdata/exemplo/ProgramaCalendario.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2016. Fábrica de Software - Instituto de Informática (UFG)
+ * Creative Commons Attribution 4.0 International License.
+ */
+
+package com.github.kyriosdata.exemplo;
+
+/**
+ * Programa que ilustra a obtenção do dia da semana
+ * para uma data específica.
+ *
+ */
+public class ProgramaCalendario
+{
+    public static void main(String[] args) {
+        System.out.println(Calendario.diaDaSemana(1, 2, 2013));
+    }
+}
